5632a4f378dc7bbcc8f21bcc11079a9f359bc060
Xoxxox21 <fauzan01062000@gmail.com>
2022-11-16 19:05:23 +0700

    get data dari py_client

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,17 @@
 from django.http import JsonResponse
+import json
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this your django API response!"})
+    # print(request.GET)
+    # print(request.POST)
+    body = request.body
+    data={}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type']=request.content_type
+    return JsonResponse(data)
