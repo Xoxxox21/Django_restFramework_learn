9ef276d9de148d1ab2ba4c5788b0ad6224e8ae2b
Xoxxox21 <fauzan01062000@gmail.com>
2022-11-16 19:48:41 +0700

    make models instace as API responce from py_client

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,7 @@
 import requests
 
 endpoint = "http://127.0.0.1:8000/api/"
-get_respons = requests.get(endpoint, params={"abc": 123}, json={"query":"Hello World"})
+get_respons = requests.get(endpoint, json={"product_id":12})
 # print(get_respons.text)
 print(get_respons.json())
 print(get_respons.status_code)
